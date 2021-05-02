20be01af6ebf0304ffac646f3d6cf11dfc92964c
hiroaki_takeoka <hiroaki.takeoka704@gmail.com>
2021-05-02 21:06:59 +0900

    cors successful4

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const httpServer = require("http").createServer();
 const io = require("socket.io")(httpServer, {
   cors: {
     //origin: "http://127.0.0.1:8080",
-    origin: "https://stoic-jackson-807da6.netlify.app",
+    origin: "*",
     methods: ["GET", "POST"]
   }
 });
